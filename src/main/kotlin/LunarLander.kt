c97eb6da5714d602e51ff39a169f9789eb2a9e9f
Zev Thompson <zevthompson@Zevs-MacBook-Air-3.local>
2026-01-12 22:26:44 -0600

    first commit

---
+++ src/main/kotlin/LunarLander.kt
@@ -0,0 +1,44 @@
+/*
+ * Author: Zev Thompson
+ */
+
+import java.util.Scanner
+
+class LunarLander(var altitude: Int=1000, var velocity: Int=40, var fuel: Int=25){
+    fun burn(fuel_requested: Int){
+        if(fuel>=fuel_requested){
+            fuel -= fuel_requested
+            velocity -= 4*fuel_requested
+            velocity += 2
+            altitude = altitude - velocity
+        }
+        else{fuel -= fuel}
+    }
+    fun status(): String{
+        return when{
+            altitude>0 -> "inflight"
+            altitude<=0 && velocity >=5 -> "crashed"
+            else -> "landed"
+        }
+    }
+}
+
+
+fun main() {
+    val lunar: LunarLander = LunarLander()
+    println(" ---- ----\nLunar Lander, now!")
+    while(lunar.status()=="inflight"){
+        println(" --")
+        println("Alt: ${lunar.altitude}, Velo: ${lunar.velocity}, Fuel: ${lunar.fuel}")
+        print("How much fuel to burn?: ")
+        lunar.burn(readln().toInt())
+    }
+    println("--")
+    when(lunar.status()){
+        "crashed" -> println("You crashed. :( Dummy")
+        "landed" -> println("You landed!")
+        else -> println("Something bizarre happened.. replay the game!")
+    }
+}
+
+// Reflection: No exact 
